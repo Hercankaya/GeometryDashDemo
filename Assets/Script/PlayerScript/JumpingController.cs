0f0c92d7feec30da57a4628e66cdb3234ed1825d
Hüsnü Ercankaya <144025899+Hercankaya@users.noreply.github.com>
2023-09-09 22:26:06 +0300

    Updated MovementController Script and JumpingController Script

--- Assets/Script/PlayerScript/JumpingController.cs
+++ Assets/Script/PlayerScript/JumpingController.cs
@@ -4,39 +4,42 @@ using UnityEngine;
 
 public class JumpingController : MonoBehaviour
 {
-
-    public float jumpForce = 10f; 
-    private bool canJump = true; 
-    private float currentSpeed;
-
+    public Transform GroundCheckTransform;
+    public float GroundCheckRadius;
+    public LayerMask GroundMask;
+    Rigidbody2D rb;
+    public Transform Sprite;
+     void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
      void Update()
     {
-        Vector2 movement = new Vector2(currentSpeed, 0);
-        transform.Translate(movement * Time.deltaTime);
+            if (OnGround())
+            {
+                Vector3 Rotation = Sprite.rotation.eulerAngles;
+                Rotation.z = Mathf.Round(Rotation.z / 90) * 90;
+                Sprite.rotation = Quaternion.Euler(Rotation);
 
-        if (Input.GetMouseButtonDown(0) && canJump)
-        {
-            Jump();
-        }
+                if (Input.GetMouseButton(0))
+                {
+                    rb.velocity = Vector2.zero;
+                    rb.AddForce(Vector2.up * 25.0f, ForceMode2D.Impulse);
+                }
+
+            }
+            else
+            {
+
+                Sprite.Rotate(Vector3.back * 450 * Time.deltaTime);
+            }
+        
     }
 
-     void Jump()
+    bool OnGround()
     {
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-        if (rb != null)
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-        }
+        return Physics2D.OverlapCircle(GroundCheckTransform.position, GroundCheckRadius, GroundMask);
+    }
 
-        canJump = false;
-    }
-    
-     void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            canJump = true;
-        }
-    }
-    
+
 }
--- Assets/Script/PlayerScript/MovementController.cs
+++ Assets/Script/PlayerScript/MovementController.cs
@@ -2,28 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Speeds { Slow = 0 , Normal = 1 , Fast = 2 };
 
 public class MovementController : MonoBehaviour
 {
-    public float startingSpeed; 
-    public float maximumSpeed; 
-    private float speedIncreaseAmount = 3.0f; 
-    public float currentSpeed;
 
-    void Start()
-    {
-        currentSpeed = startingSpeed;
-    }
+    public Speeds CurrentSpeed;
+    float[] SpeedValues = { 10.0f, 20.0f, 30.0f };
 
     void Update()
     {
-        if (currentSpeed < maximumSpeed)
-        {
-            currentSpeed += speedIncreaseAmount  * Time.deltaTime;
-        }
-
-        Vector3 movement = new Vector3(currentSpeed, 0, 0);
-        transform.Translate(movement * Time.deltaTime);
+        transform.position += Vector3.right *SpeedValues[(int)CurrentSpeed] *Time.deltaTime;
+        
     }
 
 }
