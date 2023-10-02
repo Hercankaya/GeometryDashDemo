f29453ba2cf036b1d647460d974c9519418c2587
Hüsnü Ercankaya <144025899+Hercankaya@users.noreply.github.com>
2023-10-02 18:12:00 +0300

    Slider bar created.

--- Assets/Script/GameScript/GameUIController.cs
+++ Assets/Script/GameScript/GameUIController.cs
@@ -1,23 +1,69 @@
- using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class GameUIController : MonoBehaviour
 {
-    public GameObject active;
-    public void PauseButton()
-    {
-        active.SetActive(true);
-        Time.timeScale = 0f;
 
-        
+    public GameObject PausePanel;
+    public Slider ProgressBar; 
+    public Transform StartPoint;      
+    public Transform EndPoint;
+    private Transform _playerTransform;
+    private float _totalDistance;
+
+    
+    private void Start()
+    {
+        PlayerController playerController = FindObjectOfType<PlayerController>();
+        _playerTransform = playerController.transform;
+        _totalDistance = Vector3.Distance(StartPoint.position, EndPoint.position);
     }
-    public void BackToGame()
-    {
-        active.SetActive(false);
-        Time.timeScale = 1f;
 
+    private void Update()
+    {
+        float distanceToGoal = Vector3.Distance(_playerTransform.position, EndPoint.position);
+        float progress = 1f - (distanceToGoal / _totalDistance);
+        ProgressBar.value = progress;
+
+        if (progress >= 0.999f)
+        {
+            _PauseGame();
+
+        }
+    }
+
+
+
+    private void PauseButton()
+    {
+        if (PausePanel != null)
+        {
+            PausePanel.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Debug.LogWarning("Pause Menu is not assigned!");
+        }
+    }
+
+    private void BackToGame()
+    {
+        if (PausePanel != null)
+        {
+            PausePanel.SetActive(false);
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            Debug.LogWarning("Pause Menu is not assigned!");
+        }
+    }
+    private void _PauseGame()
+    {
+        Time.timeScale = 0f;
     }
 
 }
